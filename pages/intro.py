11d0b34c2c78ad9b3db6c80488114cc524f909bf
Konstantinos Mavrakis <77228415+KostasMavrakis@users.noreply.github.com>
2025-05-28 00:50:48 +0300

    intro.py

--- pages/intro.py
+++ pages/intro.py
@@ -7,12 +7,12 @@ dash.register_page(__name__, path='/')
                 
 carousel = dbc.Carousel(
     items=[
-                    {"key": "1", "src": "/assets/2024-2025.jpg", "header": "Prague Devils", "caption": "2024 - 2025"},
-                    {"key": "2", "src": "/assets/2023-2024.jpg", "header": "Prague Devils", "caption": "2023 - 2024"},
-                    {"key": "3", "src": "/assets/2022-2023.jpg", "header": "Prague Devils", "caption": "2022 - 2023"},
-                    {"key": "4", "src": "/assets/2021-2022.jpg", "header": "Prague Devils", "caption": "2021 - 2022"},
-                    {"key": "5", "src": "/assets/2019-2020.jpg", "header": "Prague Devils", "caption": "2019 - 2020"},
-                    {"key": "6", "src": "/assets/2018-2019.jpg", "header": "Prague Devils", "caption": "2018 - 2019"},
+                    {"key": "1", "src": "/assets/photo_1.jpg", "header": "Prague Devils", "caption": "2024 - 2025"},
+                    {"key": "2", "src": "/assets/photo_2.jpg", "header": "Prague Devils", "caption": "2023 - 2024"},
+                    {"key": "3", "src": "/assets/photo_3.jpg", "header": "Prague Devils", "caption": "2022 - 2023"},
+                    {"key": "4", "src": "/assets/photo_4.jpg", "header": "Prague Devils", "caption": "2021 - 2022"},
+                    {"key": "5", "src": "/assets/photo_5.jpg", "header": "Prague Devils", "caption": "2019 - 2020"},
+                    {"key": "6", "src": "/assets/photo_6.jpg", "header": "Prague Devils", "caption": "2018 - 2019"},
     ],
     controls=True,
     indicators=True,
