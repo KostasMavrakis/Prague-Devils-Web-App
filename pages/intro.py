018e71690d1750a581203a4a09016f12c53649c8
Konstantinos Mavrakis <77228415+KostasMavrakis@users.noreply.github.com>
2025-05-28 00:36:36 +0300

    intro.py

--- pages/intro.py
+++ pages/intro.py
@@ -5,26 +5,29 @@ import dash_bootstrap_components as dbc
 # Initialize the Dash app
 dash.register_page(__name__, path='/')
                 
-layout = dbc.Container([
-    dbc.Row([
-        dbc.Col([
-            dbc.Carousel(
-                                items=[
+carousel = dbc.Carousel(
+    items=[
                     {"key": "1", "src": "/assets/2024-2025.jpg", "header": "Prague Devils", "caption": "2024 - 2025"},
                     {"key": "2", "src": "/assets/2023-2024.jpg", "header": "Prague Devils", "caption": "2023 - 2024"},
                     {"key": "3", "src": "/assets/2022-2023.jpg", "header": "Prague Devils", "caption": "2022 - 2023"},
                     {"key": "4", "src": "/assets/2021-2022.jpg", "header": "Prague Devils", "caption": "2021 - 2022"},
                     {"key": "5", "src": "/assets/2019-2020.jpg", "header": "Prague Devils", "caption": "2019 - 2020"},
                     {"key": "6", "src": "/assets/2018-2019.jpg", "header": "Prague Devils", "caption": "2018 - 2019"},
-                    ],
-                    controls=True,
-                    indicators=True,
-                    interval=2500,
-                    ride="carousel",
-                    className="carousel-fade",
-                    style={"width": "60%", "margin": "0 auto", "padding": "20px"},
-            )
-        ])
-    ])
-])
-  
+    ],
+    controls=True,
+    indicators=True,
+    interval=2500,
+    className="carousel-fade",
+    style={"width": "60%", "margin": "0 auto", "padding": "20px"},
+)
+
+layout = dbc.Container([
+    html.Div(
+        carousel,
+        style={
+            "--bs-carousel-caption-color": "white",
+            "--bs-carousel-caption-bg": "rgba(0,0,0,0.5)",
+        },
+        className="carousel-container"
+    )
+], fluid=True)
